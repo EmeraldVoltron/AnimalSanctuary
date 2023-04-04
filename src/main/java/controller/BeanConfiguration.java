7ea658a0435fad69f02a8ca0b195f60ffa860e09
xlovelyladyluck <38335658+xlovelyladyluck@users.noreply.github.com>
2023-04-04 18:51:51 -0500

    Address, Adopter, Animals classes with configurations applied. Added different packages, and bean config.

---
+++ src/main/java/controller/BeanConfiguration.java
@@ -0,0 +1,30 @@
+/**
+ * @author xbitt = twilkens
+ * CIS171 - Spring 2023
+ * Apr 4, 2023
+ */
+package controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import beans.Address;
+import beans.Adopter;
+
+@Configuration
+public class BeanConfiguration {
+	
+	@Bean
+	public Adopter adopter() {
+		Adopter bean = new Adopter("Tate Wilkens");
+		return bean;
+	}
+	
+	@Bean
+	public Address address() {
+		Address bean = new Address("2640 1st Ave S APT 110", "Altoona", "IA");
+		return bean;
+	}
+	
+
+}
